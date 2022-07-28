dffb292104693d36099355b8b16cc60d21699253
Michael Clifford <cliffom@gmail.com>
2022-07-28 15:56:00 -0700

    Added Fly benchmarks

--- fly_test.go
+++ fly_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
+// BenchmarkNewFly tests creating N flies
 func BenchmarkNewFly(b *testing.B) {
 	flies := make([]*Fly, b.N)
 	for i := 0; i < b.N; i++ {
@@ -11,6 +14,7 @@ func BenchmarkNewFly(b *testing.B) {
 	}
 }
 
+// BenchmarkFly tests animating a single fly N times
 func BenchmarkFly(b *testing.B) {
 	maxWidth := 1000
 	maxHeight := 1000
@@ -20,5 +24,26 @@ func BenchmarkFly(b *testing.B) {
 		fly.Draw()
 		fly.EdgeCheck(maxWidth, maxHeight)
 		fly.Move()
+	}
+}
+
+// BenchmarkFlies tests animating N flies [500,1000) times
+func BenchmarkFlies(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+	numFlies := rand.Intn(500) + 500
+	maxWidth := 1000
+	maxHeight := 1000
+
+	flies := make([]*Fly, numFlies)
+	for i := 0; i < numFlies; i++ {
+		flies[i] = NewFly(maxWidth, maxHeight)
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, fly := range flies {
+			fly.Draw()
+			fly.EdgeCheck(maxWidth, maxHeight)
+			fly.Move()
+		}
 	}
 }
