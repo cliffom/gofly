0d4477e0bbe70d96509a0efae12c345e7b6acc77
Michael Clifford <cliffom@gmail.com>
2022-07-29 23:42:15 -0700

    comments

--- color.go
+++ color.go
@@ -6,10 +6,12 @@ type Color struct {
 	HexValue string
 }
 
+// String returns the string representation of a color
 func (c Color) String() string {
 	return c.HexValue
 }
 
+// NewRandomColor returns a random color in hex format
 func NewRandomColor() Color {
 	return Color{
 		HexValue: randomcolor.GetRandomColorInHex(),
